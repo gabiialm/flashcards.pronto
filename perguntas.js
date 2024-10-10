370f507e69d3f936531569c5cf7e44f74c0bb235
gabii <140611693+gabiialm@users.noreply.github.com>
2024-10-10 09:17:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,19 @@
+criacartao(
+    'Esportes',
+    'Quanto tempo dura uma partida de futebol?',
+    'Uma partida de futebol dura 90 minutos divididos em dois tempos de 45 minutos.'
+)
+criacartao(
+    'Esportes',
+    'Quantos jogadores formam uma equipe de basquete?',
+    'Uma equipe de basquete tem cinco jogadores em quadra durante o jogo.'
+)
+criacartao(
+    'Esportes',
+    'Em que ano ocorreu a primeira Copa do Mundo?',
+    'A primeira Copa do Mundo aconteceu em 1930, no Uruguai.'
+)
+criacartao(
+    'Esportes',
+    
+)
